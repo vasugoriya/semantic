394a91333a2c8b9454c7703f7567a0d1436e756f
Your Name <you@example.com>
2024-06-11 15:12:17 +0530

    feat: increment release fix

--- incremental-release.js
+++ incremental-release.js
@@ -1,25 +1,35 @@
 module.exports = {
     analyzeCommits: async (pluginConfig, { commits, lastRelease }) => {
-      if (!lastRelease) {
-        // If there is no previous release, start with version 1.0.0
-        return '1.0.0';
-      }
-  
-      const { version } = lastRelease;
-      const [major, minor, patch] = version.split('.');
-  
-      // Increment the version based on the commit messages
       const releaseType = commits.some(commit => commit.type === 'feat') ? 'minor' :
                           commits.some(commit => commit.type === 'fix' || commit.type === 'perf' || commit.type === 'chore') ? 'patch' :
                           'patch';
   
-      if (releaseType === 'minor') {
-        return `${major}.${parseInt(minor) + 1}.0`;
-      } else if (releaseType === 'major') {
-        return `${parseInt(major) + 1}.0.0`;
-      } else {
-        return `${major}.${minor}.${parseInt(patch) + 1}`;
-      }
+      return releaseType;
     }
   };
+
+// module.exports = {
+//     analyzeCommits: async (pluginConfig, { commits, lastRelease }) => {
+//       if (!lastRelease) {
+//         // If there is no previous release, start with version 1.0.0
+//         return '1.0.0';
+//       }
+  
+//       const { version } = lastRelease;
+//       const [major, minor, patch] = version.split('.');
+  
+//       // Increment the version based on the commit messages
+//       const releaseType = commits.some(commit => commit.type === 'feat') ? 'minor' :
+//                           commits.some(commit => commit.type === 'fix' || commit.type === 'perf' || commit.type === 'chore') ? 'patch' :
+//                           'patch';
+  
+//       if (releaseType === 'minor') {
+//         return `${major}.${parseInt(minor) + 1}.0`;
+//       } else if (releaseType === 'major') {
+//         return `${parseInt(major) + 1}.0.0`;
+//       } else {
+//         return `${major}.${minor}.${parseInt(patch) + 1}`;
+//       }
+//     }
+//   };
   
